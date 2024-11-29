5ca975e8cde2ccc7165f3a7cb8b3d65511b6e055
Andreas Christ <andreas.christ@allane.com>
2024-11-29 10:36:10 +0100

    making test executable with UI

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,3 +1,6 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.*;
@@ -7,7 +10,10 @@ import org.junit.jupiter.api.*;
 @UsePlaywright
 public class TestExample {
     @Test
-    void test(Page page) {
+    void test() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
         page.navigate("http://lac-phpapp-s01.dc.lac.internal/php/leaseportal/login");
         page.getByPlaceholder("Benutzername").click();
         page.getByPlaceholder("Benutzername").fill("tester.haufe");
