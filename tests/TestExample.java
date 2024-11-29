c522f0de9fb4f92ef4bd8150d11a0157743280ec
Sejla Schroth <64788765+s3626@users.noreply.github.com>
2024-11-29 09:13:02 +0100

    new impl

---
+++ tests/TestExample.java
@@ -0,0 +1,19 @@
+import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.*;
+
+import org.junit.jupiter.api.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.*;
+
+@UsePlaywright
+public class TestExample {
+    @Test
+    void test(Page page) {
+        page.navigate("http://lac-phpapp-s01.dc.lac.internal/php/leaseportal/login");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Change language")).click();
+        page.getByPlaceholder("Benutzername").click();
+        page.getByPlaceholder("Benutzername").fill("tester.haufe");
+        page.getByPlaceholder("Benutzername").press("Tab");
+        page.getByPlaceholder("Passwort").fill("test1234!");
+    }
+}
